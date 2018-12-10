55d38314881b5ccb8ef32fee70682a8aa99548e8
Jahidul Islam <jahidul479@gmail.com>
2018-12-11 02:04:44 +0600

    init commit

--- src/com/jahid/ExtractHtml.java
+++ src/com/jahid/ExtractHtml.java
@@ -4,21 +4,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-public class ExtractText {
-    public static void main(String[] args){
+public class ExtractHtml {
+    public static void main(String[] args) {
 
         String html = "<html><head><title>Sample Title</title></head>"
                 + "<body>"
                 + "<p>Sample Content</p>"
                 + "<div id='sampleDiv'><a href='www.google.com'>Google</a>"
                 + "<h3><a>Sample</a><h3>"
-                +"</div>"
-                +"</body></html>";
+                + "</div>"
+                + "</body></html>";
         Document document = Jsoup.parse(html);
 
-        // a with href
+        // A with href
         Element link = document.select("a").first();
 
-        System.out.println("Text: " + link.text());
+        System.out.println("Outer Html: " + link.outerHtml());
+        System.out.println("Inner Html: " + link.html());
     }
 }

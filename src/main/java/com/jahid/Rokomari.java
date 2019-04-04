9c97ccce393dd469860c3a236fbca918b7d0c184
Jahidul Islam <jahidul479@gmail.com>
2019-04-04 13:29:24 +0600

    initial commit

--- src/main/java/com/jahid/Rokomari.java
+++ src/main/java/com/jahid/Rokomari.java
@@ -57,7 +57,7 @@ public class Rokomari {
                         Document rokomariReview = Jsoup.connect(rokomari_link + "#review").userAgent(USER_AGENT).maxBodySize(0).timeout(0).get();
                         for (Element newelement : rokomariReview.select("div.ratings-review__content--rating")) {
 
-                            String rating = newelement.getElementsByTag("p").first().text();
+                            String rating = newelement.select("h2.pt-2").text();
 
                             if (rating.contains("."))
                                 rokomari_rating = rating;
@@ -81,46 +81,7 @@ public class Rokomari {
 
                 }
 
-                /*try {
-                    Document document1 = Jsoup.connect("https://www.rokomari.com/search?term=" + title).get();
-                    for (Element rowElement : document1.select("div.book-list-wrapper")) {
-
-                        //System.out.println("row : " + document);
-
-                        rokomari_url = "https://www.rokomari.com" + rowElement.getElementsByTag("a").attr("href");
-                        String price1 = rowElement.select("p.book-price").get(0).text();
-                        String[] words = price1.split("\\s");
-
-                        rokomari_price = words[0] + words[1];
-
-                        Document newDocument1 = Jsoup.connect(rokomari_url).get();
-                        //System.out.println("newDocument: " + newDocument);
-                        for (Element element : newDocument1.select("div.details-book-main-info-wrapper")) {
-
-                            // final String newprice = element.getElementsByTag("p").get(1).text();
-                            rokomari_rating = element.getElementsByTag("span").get(2).text();
-
-                            rokomari_noOfrated = element.getElementsByTag("a").get(2).text();
-
-                            if (rokomari_rating.contains("Ratings") && rokomari_noOfrated.contains("Reviews")) {
-                                System.out.println("Rating: " + rokomari_rating);
-                                System.out.println("number_of_Rating : " + rokomari_noOfrated);
-                            } else {
-                                rokomari_rating = "";
-
-                                rokomari_noOfrated = "";
-                            }
-                            //System.out.println("new Price: " + newprice);
-                            System.out.println("rokomari  rating: " + rokomari_rating);
-                            System.out.println("rokomari Rating number: " + rokomari_noOfrated);
-
-                        }
-                        System.out.println("Title: " + rokomari_url);
-                        System.out.println("price: " + rokomari_price);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }*/
+                
 
                 resultList.add(new Book(title, author, rokomari_price, imageUrl, rokomari_link, rokomari_rating,
                         rokomari_numberOfRating, rokomari_price, rokomari_rating, rokomari_numberOfRating, rokomari_link));

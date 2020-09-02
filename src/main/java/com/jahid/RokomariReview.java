f717d1c7c0d4341f0d485241d5d7cb581d16141e
Jahidul Islam <jahidul479@gmail.com>
2020-09-02 17:38:07 +0600

    initial commit

--- src/main/java/com/jahid/RokomariReview.java
+++ src/main/java/com/jahid/RokomariReview.java
@@ -37,7 +37,7 @@ public class RokomariReview {
         FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
         driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 
-        driver.get("https://www.rokomari.com/book/author/1/%E0%A6%B9%E0%A7%81%E0%A6%AE%E0%A6%BE%E0%A7%9F%E0%A7%82%E0%A6%A8-%E0%A6%86%E0%A6%B9%E0%A6%AE%E0%A7%87%E0%A6%A6?ref=mm_p0&page=4");
+        driver.get("https://www.rokomari.com/book/author/1/%E0%A6%B9%E0%A7%81%E0%A6%AE%E0%A6%BE%E0%A7%9F%E0%A7%82%E0%A6%A8-%E0%A6%86%E0%A6%B9%E0%A6%AE%E0%A7%87%E0%A6%A6?ref=mm_p0&page=5");
         int i = 0;
         try {
 
@@ -69,7 +69,7 @@ public class RokomariReview {
 
 
                 resultList.add(new BookReview(title, summary, review));
-                OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(new File("book_review_9.json"), resultList);
+                OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(new File("book_review_10.json"), resultList);
                 reviewDriver.quit();
             }
 
